369e861f0e0f9e6f306f1ea55fd2dc5527832e8b
Some User <Ditto2X@github.com>
2016-12-30 14:07:09 -0800

    Added some more argument handling as well as date calculations.

--- python/storm/stats/statsparser.py
+++ python/storm/stats/statsparser.py
@@ -11,17 +11,24 @@ class Statsparser(object) :
 
   def parse(self) :
     """ Parses command line arguments. """
-    parser = ArgumentParser()
+    parser = ArgumentParser(usage='%(prog)s [options] (--ingestion|--hold)')
+#     parser.add_argument('-f', '--file', action = 'store', dest = 'file', type = FileType('r'), required = True,
+#       help = 'Use file for extracting stats data.')
+    parser.add_argument('-f', '--file', nargs = '+', type = FileType('r'), required = True,
+      help = 'Use file(s) for extracting stats data.')
     parser.add_argument('--debug', action='store_true', default=False, dest='debug',
       help = 'Set DEBUG flag for extra output.')
-    parser.add_argument('-f', '--file', action = 'store', dest = 'file', type = FileType('r'),
-      help = 'Use file for extracting stats data.')
     parser.add_argument('--start', action = 'store', dest = 'start', default = datetime(1970,1,1,0,0,0),
       type = lambda d: datetime.strptime(d, '%Y-%m-%dT%H:%M:%S'),
       help = 'Extract rates after start time. (YYYY-MM-DDTHH:MM:SS)')
     parser.add_argument('--stop', action = 'store', dest = 'stop',
       type = lambda d: datetime.strptime(d + ':999999', '%Y-%m-%dT%H:%M:%S:%f'), default = None,
       help = 'Extract rates before stop time. (YYYY-MM-DDTHH:MM:SS)')
+    group = parser.add_mutually_exclusive_group(required = True)
+    group.add_argument('--ingestion', action = 'store_const', dest = 'action', const = 'IngestionSpout',
+      help = 'Process IngestionSpout entries.')
+    group.add_argument('--hold', action = 'store_const', dest = 'action', const = 'HoldSpout',
+      help = 'Process HoldSpout entries.')
     self.options = parser.parse_args()
 
     if (self.options.stop != None) and (self.options.start > self.options.stop) :
@@ -67,7 +74,7 @@ class Statsparser(object) :
       (?P<time>\d{2}:\d{2}:\d{2}:\d{3})\s+
       (?P<host>\S+)\s+
       \[THREAD\sID=(?P<thread>.*)\]\s+
-      AlcatrazStats\s+
+      (?P<tag>\S+)\s+
       \[(?P<loglevel>.*)\]\s+
       %s
       """ % entry, re.VERBOSE)
@@ -79,19 +86,26 @@ class Statsparser(object) :
       return None
 
   def process_file(self, spout) :
-    with self.options.file as source :
+    for source in self.options.file :
+      total = 0
+      begin,end = None, None
       for stuff in self.group_by_date(source) :
         header = self.is_entry(spout, stuff[0])
         if header :
           date = datetime.strptime("%sT%s" % (header['date'], header['time']), '%Y-%m-%dT%H:%M:%S:%f')
           stats = self.get_rate(stuff[1:])
           if stats and date > self.options.start and ((self.options.stop == None) or (date < self.options.stop)) :
-            print "%s: %d" % (date.strftime("%Y-%m-%d %H:%M:%S"), int(stats['total']))
+            if begin == None or date < begin :
+              begin = date
+            if end == None or date > end :
+              end = date
+            total += int(stats['total'])
+            # print "%s: %d" % (date.strftime("%Y-%m-%d %H:%M:%S"), int(stats['total']))
+      print "Total: %d, Diff = %s" % (total, end - begin)
 
   def main(self) :
     self.parse()
-    # self.process_file('IngestionSpout')
-    self.process_file('HoldSpout')
+    self.process_file(self.options.action)
 
 if __name__ == "__main__" :
   app = Statsparser()
